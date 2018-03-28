057b939aa6a240e3e6f60dcf37fae14e17711415
Benjamin H Boruff <ben@boruff.me>
2018-03-28 11:07:53 -0400

    sorry, I really like semis

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { microGraphiql, microGraphql } from 'apollo-server-micro'
-import { send } from 'micro'
-import { get, post, router } from 'microrouter'
-import schema from './schema/books.js'
+import { microGraphiql, microGraphql } from 'apollo-server-micro';
+import { send } from 'micro';
+import { get, post, router } from 'microrouter';
+import schema from './schema/books.js';
 
-const graphqlHandler = microGraphql({ schema })
-const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' })
+const graphqlHandler = microGraphql({ schema });
+const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' });
 
 export default router(
   get('/graphql', graphqlHandler),
   post('/graphql', graphqlHandler),
   get('/graphiql', graphiqlHandler),
   (req, res) => send(res, 404, 'not found')
-)
+);
