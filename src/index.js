178bf0f70c4f49edc2955b4f2a3d50551ef63c89
Benjamin H Boruff <ben@boruff.me>
2018-04-03 20:01:10 -0400

    fixed all eslint/google issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { microGraphiql, microGraphql } from 'apollo-server-micro';
-import { send } from 'micro';
-import { get, post, router } from 'microrouter';
+import {microGraphiql, microGraphql} from 'apollo-server-micro';
+import {send} from 'micro';
+import {get, post, router} from 'microrouter';
 import schema from './schema/books.js';
 
-const graphqlHandler = microGraphql({ schema });
-const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' });
+const graphqlHandler = microGraphql({schema});
+const graphiqlHandler = microGraphiql({endpointURL: '/graphql'});
 
 export default router(
   get('/graphql', graphqlHandler),
--- src/resolvers/books.js
+++ src/resolvers/books.js
@@ -1,18 +1,18 @@
 // Some fake data
 const books = [
   {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling'
+    title: 'Harry Potter and the Sorcerer\'s stone',
+    author: 'J.K. Rowling',
   },
   {
     title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
+    author: 'Michael Crichton',
+  },
 ];
 
 // The resolvers
 const resolvers = {
-  Query: { books: () => books }
+  Query: {books: () => books},
 };
 
 export default resolvers;
