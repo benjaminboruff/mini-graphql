b082a0bf86365544f9fa4d5016952809738d7502
Benjamin H Boruff <ben@boruff.me>
2018-04-04 21:01:04 -0400

    adding new schema,resolvers, and tests

--- src/resolvers/books.js
+++ src/resolvers/books.js
@@ -1,18 +1,50 @@
 // Some fake data
 const books = [
   {
+    id: 1,
     title: 'Harry Potter and the Sorcerer\'s stone',
-    author: 'J.K. Rowling',
+    authorId: 1,
   },
   {
+    id: 2,
     title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    authorId: 2,
+  },
+];
+
+const authors = [
+  {
+    id: 1,
+    firstName: 'J.K.',
+    lastName: 'Rowling',
+  },
+  {
+    id: 2,
+    firstName: 'Michael',
+    lastName: 'Crichton',
   },
 ];
 
 // The resolvers
 const resolvers = {
-  Query: {books: () => books},
+  Query: {
+    allBooks: () => books,
+    allAuthors: () => authors,
+    author: (obj, {id}) => authors.find((author) => author.id === id),
+    book: (obj, {id, title}) => books
+      .find((book) => {
+        return (
+        id && title ? book.id === id && book.title === title :
+          id ? book.id === id : title ? book.title === title : null
+      );
+    }),
+  },
+  Author: {
+    books: (author) => books.filter((book) => book.authorId === author.id),
+  },
+  Book: {
+    author: (book) => authors.find((author) => author.id === book.authorId),
+  },
 };
 
 export default resolvers;
--- src/schema/books.js
+++ src/schema/books.js
@@ -1,10 +1,28 @@
-import {makeExecutableSchema} from 'graphql-tools';
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools';
 import resolvers from '../resolvers/books';
+import mocks from './mocks';
 
 // The GraphQL schema in string form
 const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
+  type Query {
+    allBooks: [Book]
+    allAuthors: [Author]
+    author(id: Int): Author
+    book(id: Int, title: String): Book
+  }
+
+  type Author {
+    id: Int!
+    firstName: String
+    lastName: String
+    books: [Book]
+  }
+
+  type Book {
+    id: Int!
+    title: String!
+    author: Author
+  }
 `;
 
 // Put together a schema
@@ -12,5 +30,7 @@ const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
+
+// addMockFunctionsToSchema({schema, mocks});
 
 export default schema;
